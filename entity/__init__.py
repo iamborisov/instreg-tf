7705b0ba9ea1ce6a4be298f6de668aa4bcc25a80
iamborisov <iamborisov@gmail.com>
2019-06-23 01:39:15 +0300

    Entities

---
+++ entity/__init__.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from entity.country import *
+from entity.institution import *
+from entity.property import *
